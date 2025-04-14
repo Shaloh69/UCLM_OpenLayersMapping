c528996b612bdecd340492961ba70b463eeba101
Shaloh69 <dumporshemjoshua@gmail.com>
2025-04-14 09:23:25 +0800

    Ngrok Implementation Phone - Routing needs bug fixing in some locations

--- kiosk_Electron/preload.js
+++ kiosk_Electron/preload.js
@@ -1,17 +1,17 @@
-//preload.js
 const { contextBridge, ipcRenderer } = require("electron");
 
-// Expose APIs to renderer process
 contextBridge.exposeInMainWorld("electron", {
-  // Get the ngrok URL (using the function name your code is expecting)
   getNgrokUrl: () => ipcRenderer.invoke("get-server-url"),
-
-  // Set the ngrok URL
   updateNgrokUrl: (url) => ipcRenderer.invoke("set-server-url", url),
-
-  // Reload the main window
   reloadApp: () => ipcRenderer.send("reload-main-window"),
-
-  // Flag to indicate we're running in Electron
   isElectron: true,
+
+  getCurrentLocation: () =>
+    new Promise((resolve, reject) => {
+      if (!navigator.geolocation) {
+        reject(new Error("Geolocation not supported"));
+      } else {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      }
+    }),
 });
